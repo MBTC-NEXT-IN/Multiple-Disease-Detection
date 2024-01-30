3cf9572c5265469e339d2e72900bd6df3c2acefd
Nikhil Patel <nikhilpatel26150@gmail.com>
2024-01-30 21:20:25 +0530

    App Created & Developement start from 1 Feb 2024

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to the development of Multiple Disease Detection System 
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/MBTC-NEXT-IN/multiple-disease-detection"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Project Development Starting from 1 Feb 2024
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
